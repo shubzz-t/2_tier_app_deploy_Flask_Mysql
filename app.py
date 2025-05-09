2802bd9ea9b5ab5a9650e9219bc82f0cebdc494b
Shubham Taware <93321554+shubzz-t@users.noreply.github.com>
2025-05-09 07:08:02 +0530

    Update app.py
    port added

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # Configure MySQL from environment variables
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
+app.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT', 3306)
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
 
